163cf8591aff7fe458920b0c4a4be29b78a3e467
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-08-09 15:52:32 +0200

    mf

--- src/main/python/run_evaluation_task.py
+++ src/main/python/run_evaluation_task.py
@@ -12,6 +12,12 @@ if 'src/main/python/' not in sys.path:
     sys.path.append('src/main/python/')
 
 from run_file_processing import IncompletePools, load_all_runs, normalize_run
+from evaluation_util import evaluate_on_pools
+
+
+def normalize_identifier(identifier):
+    return identifier.replace('/', '-')
+
 
 def __parse_args():
     parser = argparse.ArgumentParser(description='Evaluate runs.')
@@ -31,8 +37,16 @@ def run_task(task):
         return
     
     subprocess.check_output(['mkdir', '-p', os.path.abspath(str(Path(task['working_directory'] + '/' + task['out_file_name'])/ '..'))])
+    
+    qrel_file = 'src/main/resources/unprocessed/topics-and-qrels/qrels.robust04.txt'
+    pooling = IncompletePools(pool_per_run_file=task['working_directory'] + '/processed/pool-documents-per-run-' + normalize_identifier(task['trec_identifier']) + '.json.gz')
+    pools = {k:set(v) for k,v in pooling.create_incomplete_pools_for_run(task['run'])}
+    
+    eval_result = evaluate_on_pools(task['run'], qrel_file, pools, task['measure'])
+    eval_result['task'] = task
+    
     with open(out_file, 'w') as f:
-        f.write(json.dumps(task))
+        f.write(json.dumps(eval_result))
 
 
 if __name__ == '__main__':
