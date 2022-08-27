cd5c132f04fcb4b18150d623702588ab3e0b43de
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-08-27 23:33:08 +0200

    prepare object oriented implementation of bootstrapping

--- src/main/python/bootstrap_util.py
+++ src/main/python/bootstrap_util.py
@@ -5,6 +5,8 @@ from copy import deepcopy
 from numpy import isnan
 from trectools import TrecQrel, TrecEval, TrecRun
 
+from fast_ndcg_implementation import MemorizedNdcg
+
 
 def __unjudged_documents(run, qrels):
     ret = pd.merge(run, qrels[["query","docid","rel"]], how="left")
@@ -140,4 +142,55 @@ def evaluate_bootstrap(run, qrels, measure, repeat=5, seed=1):
             ret[topic][measure] += [substitute_pools[topic][bootstrap]]
 
     return ret
+
+
+class BootstrappEval:
+    def __init__(self, measure):
+        self.__measure = measure
+        self.__depth = int(measure.split('@')[-1])
+
+        if self.__measure.lower().startswith('ndcg@'):
+            raise ValueError('Invalid Measure')
+        self.__evaluator = MemorizedNdcg(self.__depth)
+
+    def bootstrap(self, run, qrels, measure, repeat=5, seed=1):
+        run = normalize_run(run, self.__depth)
+        ret = {}
+
+        for topic in tqdm(qrels['query'].unique()):
+            assert topic not in ret
+            ret[topic] = {measure: []}
+            run_for_topic = run.run_data[run.run_data['query'] == topic]
+            eval_scores = self.__eval_scores(run_for_topic, qrels)
+
+            for bootstrap in self.__bootstraps_for_topic(run_for_topic, qrels.qrels_data, seed=seed, repeat=repeat):
+                ret[topic][measure] += [eval_scores[topic][bootstrap]]
+
+    def __bootstraps_for_topic(self, run_for_topic, qrels, seed, repeat):
+        return []
+
+
+    def __eval_scores(self, run, qrels):
+        ret = {}
+        memorized_ndg_scores = self.__evaluator.get_ndcg(run, qrels, self.__depth)
+
+        if type(qrels) == TrecQrel:
+            qrels = qrels.qrels_data
+
+        for topic in qrels['query'].unique():
+            assert topic not in ret
+            incomplete_ndcg = memorized_ndg_scores[topic]
+
+            doc_to_qrel = {}
+            for _, i in qrels[qrels['query'] == topic].iterrows():
+                if i['docid'] in doc_to_qrel:
+                    raise ValueError(f'I do not know how to handle duplicates in qrels: docid={i["docid"]} topic={topic}')
+
+                doc_to_qrel[i['docid']] = i['rel']
+
+            incomplete_ndcg.set_doc_to_qrel(doc_to_qrel)
+
+            ret[topic] = incomplete_ndcg
+
+        return ret
 
